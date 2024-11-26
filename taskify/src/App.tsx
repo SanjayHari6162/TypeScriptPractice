0cba6a844beb6b62d21ca06f4f88750b9e61f993
SanjayHari6162 <sanjayhari6162@gmail.com>
2024-11-26 10:52:10 +0000

    started

---
+++ taskify/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { InputContainer } from './InputContainer';
+
+const App =()=> {
+  return (
+    <>
+     <h1 className="heading">Taskify</h1>
+     < InputContainer />
+    
+    </>
+  );
+}
+
+export default App;
---
+++ taskify/src/InputContainer.tsx
@@ -0,0 +1,12 @@
+
+
+export const InputContainer = () => {
+    return (
+        <>
+             <div className="input-container">
+                <input type="text" className='input-element'/>
+                <button type='submit' className='submit-button'> Add</button>
+            </div>
+        </>
+    );
+};
